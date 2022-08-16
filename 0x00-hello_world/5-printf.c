8356c12c6d961091605283be8951fba2860e73b7
Deji287 <slimdejavu0101@gmail.com>
2022-08-16 10:22:04 +0100

    C program that uses printf to display input

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*A code that prints input using printf
+*Return: Always 0 on success
+*/
+int main(void)
+{
+	printf("What you see is what you get\n");
+	return (0);
+}
