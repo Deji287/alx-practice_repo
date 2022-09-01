219753b8a83d63afe47d963f496f0b99471a6c41
Deji287 <slimdejavu0101@gmail.com>
2022-09-01 07:29:35 +0100

    code to check for upper case letter

---
+++ 0x03-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*_isupper - code to check for upper case letter"
+*@c: char
+*Return: 0 always
+*/
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	return (1);
+
+	else
+	return (0);
+}
