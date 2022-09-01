9c0de1c13a4e4f9c6d1f400611eddf1c565aba77
Deji287 <slimdejavu0101@gmail.com>
2022-09-01 08:10:07 +0100

    code to return multiplication of two numbers

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+*mul - code to calculate product of two numbers
+*@a - first int
+*@b - second int
+*Return: 0 on success
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
